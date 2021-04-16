b8aefd094a18768f5c03478955a9260c72d1c96b
chloedunne <chloedunne1@gmail.com>
2021-04-16 12:24:37 +0100

    created main page

--- app/src/main/java/com/example/electronicsstore/MainActivity.java
+++ app/src/main/java/com/example/electronicsstore/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.electronicsstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signUp = (Button) findViewById(R.id.signUpButton);
+        Button logIn = (Button) findViewById(R.id.loginButton);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
